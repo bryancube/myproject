afa9bc1d499f45749c2f2d0cd975dd0359d76e30
kesh1i <jjung080510@gmail.com>
2025-03-08 16:21:02 +0900

    express 사용

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const http = require('http');
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end('안녕, 세상!\n');
-}).listen(80, () =>{
-    console.log('80번 포트에서 서버 실행');
-});
+const port = 80;
+const express = require('express')
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+})
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
